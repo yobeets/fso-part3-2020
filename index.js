7ebcd2c7bd5a9351889685831c002d749ec5e362
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2020-05-11 13:44:50 +1000

    completed 3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token('data', function(req, res) {
+	return JSON.stringify(req.body)
+})
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 let persons = [
     {
