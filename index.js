eb09b684ca1c4cf088d188396b520757ea57af38
Mitchell Bartolo <mitchell.bartolo@gmail.com>
2020-05-11 11:45:13 +1000

    completed 3.6

--- index.js
+++ index.js
@@ -29,6 +29,62 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.get('/info', (req, res) => {
+    const peopleCount = persons.length
+    const timeStamp = new Date()
+    //console.log(peopleCount)
+    res.send(`<p>Phonebook has info for number ${peopleCount} people</p><p>${timeStamp}</p>`)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const randomId = Math.floor(Math.random() * 6000) + 1  
+    return randomId
+    }
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name || !body.number) {
+        return res.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return res.status(400).json({
+            error: 'person already listed in phonebook'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
